52eccb592b912e358fecbe5586d964f0eca6504e
Andrew McKinlay <apmckinlay@gmail.com>
2020-05-10 19:40:51 -0600

    change heading font to Kalam

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("fonts");
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("photos");
     return {
